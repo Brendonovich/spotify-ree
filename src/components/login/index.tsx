e611dec285e92b1b81de83d947ddc582e470c0c1
Brendan Allan <brendonovich@outlook.com>
2021-02-11 12:31:43 +0800

    fix for vercel

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -2,11 +2,11 @@ const Login = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <button
-        onClick={() =>
-          fetch({
-            url: "https://us-central1-spotify-ree.cloudfunctions.net/redirect",
-          })
-        }
+        // onClick={() =>
+        //   fetch({
+        //     url: "https://us-central1-spotify-ree.cloudfunctions.net/redirect",
+        //   })
+        // }
         className="p-4 rounded-md bg-black text-spotify-green text-2xl hover:bg-gray-900"
       >
         Login With Spotify
