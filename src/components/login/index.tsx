985436dd93a98730c0ed4a103011bb11342cbf77
Brendan Allan <brendonovich@outlook.com>
2021-02-11 12:28:24 +0800

    inital commit

---
+++ src/components/login/index.tsx
@@ -0,0 +1,18 @@
+const Login = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <button
+        onClick={() =>
+          fetch({
+            url: "https://us-central1-spotify-ree.cloudfunctions.net/redirect",
+          })
+        }
+        className="p-4 rounded-md bg-black text-spotify-green text-2xl hover:bg-gray-900"
+      >
+        Login With Spotify
+      </button>
+    </div>
+  );
+};
+
+export default Login;
